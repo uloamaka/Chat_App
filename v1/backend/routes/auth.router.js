495258490a46eee64ca612c1ad5397bd54811361
Ebite God'sgift Uloamaka <godsgiftuloamaka235@gmail.com>
2023-12-17 12:29:56 +0100

    controllers setup

---
+++ v1/backend/routes/auth.router.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const { adminAuth } = require("../middlewares/auth");
+
+const registerUser = require("../controllers/auth.controller");
+
+router.post("/register", registerUser.registerUser);
+
+router.post("/login", registerUser.loginUser);
+
+router.patch("/update-role", adminAuth, registerUser.updateUserRole);
+
+router.delete("/delete-user", adminAuth, registerUser.deleteUser);
+
+router.post("/forget-password", registerUser.forgotPassword);
+
+router.post("/reset-password/:userId/:resetToken", registerUser.resetPassword);
+
+
+module.exports = router;
