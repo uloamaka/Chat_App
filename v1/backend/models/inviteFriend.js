02ef2b94eca1e21149f62f9996807dd906be09af
Ebite God'sgift Uloamaka <godsgiftuloamaka235@gmail.com>
2023-12-18 03:09:01 +0100

    Set up Mongoose model for chat

---
+++ v1/backend/models/inviteFriend.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const inviteSchema = new Schema(
+  {
+    sender: { type: Schema.Types.ObjectId, ref: "User" },
+    receiver: { type: String },
+    inviteStatus: { type: String, enum: ["accepted", "pending"] },
+    inviteToken: { type: String },
+  },
+  { timestamps: true }
+);
+
+const InviteFriend = mongoose.model("InviteFriend", inviteSchema);
+module.exports = InviteFriend;
